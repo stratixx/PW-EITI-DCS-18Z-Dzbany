f0b1cc1985d49dc117d285eb1e49b81aaf4b41a9
Krystian Gulinski <gulinski.krystian@gmail.com>
2018-12-07 18:05:50 +0100

    add some run scripts

---
+++ run_all.m
@@ -0,0 +1,5 @@
+run('materialy/krystian_plots/runMe.m');
+% run('materialy/po labie dane - skoki z regulatorem/runMe.m');
+% run('materialy/dzbany - czyli chyba pierwsze spotkanie/runMe.m');
+% run('materialy/dcs_dane_po_konkursie - skoki sterowania i cos jeszcze/runMe.m');
+% run('materialy/dane - skoki sterowania/runMe.m');
